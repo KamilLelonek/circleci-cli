6f6007b6d15d6e7561fd0d61d4c1973d51067b36
Kamil Lelonek <squixy.sln@gmail.com>
2015-06-28 00:03:53 +0200

    Fixed interpolation variables

--- lib/circleci_cli/build/endpoints.ex
+++ lib/circleci_cli/build/endpoints.ex
@@ -1,18 +1,18 @@
 defmodule CircleciCli.Build.Endpoints do
   def details(user, project, build_number) do
-    "/project/#{user}/#{project}/#{build_num}"
+    "/project/#{user}/#{project}/#{build_number}"
   end
 
   def artifacts(user, project, build_number) do
-    "/project/#{user}/#{project}/#{build_num}/artifacts"
+    "/project/#{user}/#{project}/#{build_number}/artifacts"
   end
 
   def retry(user, project, build_number) do
-    "/project/#{user}/#{project}/#{build_num}/retry"
+    "/project/#{user}/#{project}/#{build_number}/retry"
   end
 
   def cancel(user, project, build_number) do
-    " /project/#{user}/#{project}/#{build_num}/cancel"
+    " /project/#{user}/#{project}/#{build_number}/cancel"
   end
 
   def trigger(user, project, branch) do
