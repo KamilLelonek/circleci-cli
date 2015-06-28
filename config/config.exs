542f2951fafcae09e6387918d9f920378aab5631
Kamil Lelonek <squixy.sln@gmail.com>
2015-06-28 12:35:21 +0200

    Configuring logger
    In development or tests should print all messages while in production
    only error ones.

--- config/config.exs
+++ config/config.exs
@@ -1,7 +1,9 @@
 use Mix.Config
 
-import_config "#{Mix.env}.exs"
-
 config :circleci_cli,
        endpoint: "https://circleci.com/api/v1/",
        token:    System.get_env("CIRCLECI_TOKEN")
+
+config :logger, compile_time_purge_level: :debug
+
+import_config "#{Mix.env}.exs"
--- config/prod.exs
+++ config/prod.exs
@@ -1,1 +1,3 @@
 use Mix.Config
+
+config :logger, compile_time_purge_level: :error
