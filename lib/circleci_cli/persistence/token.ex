2d56ebd9cf296910f0ddf09b37fc1566a579a969
Kamil Lelonek <squixy.sln@gmail.com>
2015-07-02 18:17:34 +0200

    Reorganised code and renamed variables

--- lib/circleci_cli/persistence/token.ex
+++ lib/circleci_cli/persistence/token.ex
@@ -2,10 +2,8 @@ defmodule CircleciCli.Persistence.Token do
   @file_name ".circle_cli_token"
   @env_name  "CIRCLECI_API_TOKEN"
 
-  defp full_path, do: "#{System.user_home}/#{@file_name}"
-
-  def write(token), do: File.write(full_path, token)
-  def read,         do: read_token_from_file File.read(full_path)
+  def  write(token), do: File.write(token_path, token)
+  def  read,         do: read_token_from_file File.read(token_path)
 
   defp read_token_from_file({ :error, _ }) do
     case token_from_env do
@@ -14,6 +12,7 @@ defmodule CircleciCli.Persistence.Token do
     end
   end
 
-  defp read_token_from_file({ :ok, token }), do: { :ok, token }
+  defp token_path,                           do: "#{System.user_home}/#{@file_name}"
   defp token_from_env,                       do: System.get_env @env_name
+  defp read_token_from_file({ :ok, token }), do: { :ok, token }
 end
