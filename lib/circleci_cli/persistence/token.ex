c55d988028f568e98ffe904b1ecb4f52099013d3
Kamil Lelonek <squixy.sln@gmail.com>
2015-07-02 15:56:22 +0200

    Reading token from environmental variable

--- lib/circleci_cli/persistence/token.ex
+++ lib/circleci_cli/persistence/token.ex
@@ -1,13 +1,19 @@
 defmodule CircleciCli.Persistence.Token do
   @file_name ".circle_cli_token"
+  @env_name  "CIRCLECI_API_TOKEN"
 
   defp full_path, do: "#{System.user_home}/#{@file_name}"
 
-  def write(token) do
-    File.write full_path, token
+  def write(token), do: File.write(full_path, token)
+  def read,         do: read_token_from_file File.read(full_path)
+
+  defp read_token_from_file({ :error, _ }) do
+    case token_from_env do
+      nil   -> { :error, "Neither file nor environmental variable with token was found."}
+      token -> { :ok, token}
+    end
   end
 
-  def read do
-    File.read full_path
-  end
+  defp read_token_from_file({ :ok, token }), do: { :ok, token }
+  defp token_from_env,                       do: System.get_env @env_name
 end
