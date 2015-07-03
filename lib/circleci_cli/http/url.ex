0d6957d009225a8e85ef09b9b7fd9484921d5b09
Kamil Lelonek <squixy.sln@gmail.com>
2015-07-03 12:45:24 +0200

    Making public token build

--- lib/circleci_cli/http/url.ex
+++ lib/circleci_cli/http/url.ex
@@ -2,7 +2,7 @@ defmodule CircleciCli.HTTP.URL do
   @circleci_endpoint             "https://circleci.com/api/v1"
   @circleci_endpoint_token_query "?circle-token="
 
-  defp build(endpoint, token) do
+  def build(endpoint, token) do
     "#{@circleci_endpoint}#{endpoint}#{@circleci_endpoint_token_query}#{token}"
   end
 end
