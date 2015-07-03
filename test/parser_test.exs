390d19a17d60f3e068b176ad33bf068ea21a3d73
Kamil Lelonek <squixy.sln@gmail.com>
2015-07-03 09:40:19 +0200

    Simplified tests

--- test/parser_test.exs
+++ test/parser_test.exs
@@ -4,71 +4,86 @@ defmodule CircleciCli.ParserTest do
   require CircleciCli.Parser, as: Praser
 
   test "help command" do
-    assert Praser.parse_args("", ["--help"]) == :help
-    assert Praser.parse_args("", ["--h"])    == :help
+    assert_parse ["--help"], :help
+    assert_parse ["--h"],    :help
   end
 
   test "user command" do
-    assert Praser.parse_args("", ["user"])                  == {:user, ""}
-    assert Praser.parse_args("", ["user", "--token=token"]) == {:user, ""}
+    result = {:user, ""}
+    assert_parse ["user"],                  result
+    assert_parse ["user", "--token=token"], result
   end
 
   test "projects command" do
-    assert Praser.parse_args("", ["projects"])                  == {:projects, ""}
-    assert Praser.parse_args("", ["projects", "--token=token"]) == {:projects, ""}
+    result = {:projects, ""}
+    assert_parse ["projects"],                  result
+    assert_parse ["projects", "--token=token"], result
   end
 
   test "builds command" do
-    assert Praser.parse_args("", ["builds"]) == {:recent_builds, ""}
+    assert_parse ["builds"], {:recent_builds, ""}
   end
 
   test "clear-cache command" do
-    assert Praser.parse_args("", ["clear-cache", "--user=user", "--project=project"]) == {:project_clear_cache, "", "user", "project"}
-    assert Praser.parse_args("", ["clear-cache", "-u", "user", "-p", "project"])      == {:project_clear_cache, "", "user", "project"}
+    result = {:project_clear_cache, "", "user", "project"}
+    assert_parse ["clear-cache", "--user=user", "--project=project"], result
+    assert_parse ["clear-cache", "-u", "user", "-p", "project"],      result
   end
 
   test "build command" do
-    assert Praser.parse_args("", ["build", "--user=user", "--project=project", "--build=1"]) == {:project_build, "", "user", "project", 1}
-    assert Praser.parse_args("", ["build", "-u", "user", "-p", "project", "-n", "1"])        == {:project_build, "", "user", "project", 1}
+    result = {:project_build, "", "user", "project", 1}
+    assert_parse ["build", "--user=user", "--project=project", "--build=1"], result
+    assert_parse ["build", "-u", "user", "-p", "project", "-n", "1"],        result
   end
 
   test "artifacts command" do
-    assert Praser.parse_args("", ["artifacts", "--user=user", "--project=project", "--build=1"]) == {:project_build_artifacts, "", "user", "project", 1}
-    assert Praser.parse_args("", ["artifacts", "-u", "user", "-p", "project", "-n", "1"])        == {:project_build_artifacts, "", "user", "project", 1}
+    result = {:project_build_artifacts, "", "user", "project", 1}
+    assert_parse ["artifacts", "--user=user", "--project=project", "--build=1"], result
+    assert_parse ["artifacts", "-u", "user", "-p", "project", "-n", "1"],        result
   end
 
   test "retry command" do
-    assert Praser.parse_args("", ["retry", "--user=user", "--project=project", "--build=1"]) == {:project_build_retry, "", "user", "project", 1}
-    assert Praser.parse_args("", ["retry", "-u", "user", "-p", "project", "-n", "1"])        == {:project_build_retry, "", "user", "project", 1}
+    result = {:project_build_retry, "", "user", "project", 1}
+    assert_parse ["retry", "--user=user", "--project=project", "--build=1"], result
+    assert_parse ["retry", "-u", "user", "-p", "project", "-n", "1"],        result
   end
 
   test "cancel command" do
-    assert Praser.parse_args("", ["cancel", "--user=user", "--project=project", "--build=1"]) == {:project_build_cancel, "", "user", "project", 1}
-    assert Praser.parse_args("", ["cancel", "-u", "user", "-p", "project", "-n", "1"])        == {:project_build_cancel, "", "user", "project", 1}
+    result = {:project_build_cancel, "", "user", "project", 1}
+    assert_parse ["cancel", "--user=user", "--project=project", "--build=1"], result
+    assert_parse ["cancel", "-u", "user", "-p", "project", "-n", "1"],        result
   end
 
   test "trigger command" do
-    assert Praser.parse_args("", ["trigger", "--user=user", "--project=project", "--branch=master"]) == {:project_build_trigger, "", "user", "project", "master"}
-    assert Praser.parse_args("", ["trigger", "-u", "user", "-p", "project", "-b", "master"])         == {:project_build_trigger, "", "user", "project", "master"}
+    result = {:project_build_trigger, "", "user", "project", "master"}
+    assert_parse ["trigger", "--user=user", "--project=project", "--branch=master"], result
+    assert_parse ["trigger", "-u", "user", "-p", "project", "-b", "master"],         result
   end
 
   test "project-key command" do
-    assert Praser.parse_args("", ["project-key", "--user=user", "--project=project", "--key=key"]) == {:project_add_ssh_key, "", "user", "project", "key"}
-    assert Praser.parse_args("", ["project-key", "-u", "user", "-p", "project", "-k", "key"])      == {:project_add_ssh_key, "", "user", "project", "key"}
+    result = {:project_add_ssh_key, "", "user", "project", "key"}
+    assert_parse ["project-key", "--user=user", "--project=project", "--key=key"], result
+    assert_parse ["project-key", "-u", "user", "-p", "project", "-k", "key"],      result
   end
 
   test "user-key command" do
-    assert Praser.parse_args("", ["user-key", "--key=key"]) == {:user_add_ssh_key, "", "key"}
-    assert Praser.parse_args("", ["user-key", "-k", "key"]) == {:user_add_ssh_key, "", "key"}
+    result = {:user_add_ssh_key, "", "key"}
+    assert_parse ["user-key", "--key=key"], result
+    assert_parse ["user-key", "-k", "key"], result
   end
 
   test "heroku-key command" do
-    assert Praser.parse_args("", ["heroku-key", "--key=key"]) == {:user_add_heroku_key, "", "key"}
-    assert Praser.parse_args("", ["heroku-key", "-k", "key"]) == {:user_add_heroku_key, "", "key"}
+    result = {:user_add_heroku_key, "", "key"}
+    assert_parse ["heroku-key", "--key=key"], result
+    assert_parse ["heroku-key", "-k", "key"], result
   end
 
   test "any other command" do
-    assert Praser.parse_args("", [])           == :help
-    assert Praser.parse_args("", ["whatever"]) == :help
+    assert_parse [],           :help
+    assert_parse ["whatever"], :help
+  end
+
+  defp assert_parse(args, result) do
+    assert Praser.parse_args("", args) == result
   end
 end
