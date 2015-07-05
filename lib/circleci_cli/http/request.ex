dc36132e8e8189c34ce81d68f9608149f34c845e
Kamil Lelonek <squixy.sln@gmail.com>
2015-07-05 15:51:04 +0200

    Formatting HTTP result

--- lib/circleci_cli/http/request.ex
+++ lib/circleci_cli/http/request.ex
@@ -1,16 +1,21 @@
 defmodule CircleciCli.HTTP.Request do
+  @headers [{:Accept, "application/json"}]
+
   def send([url: url, method: method]),
   do: send([url: url, method: method, body: ""])
 
   def send([url: url, method: method, body: body]),
-  do: HTTPoison.request(method, url, body)
+  do: HTTPoison.request(method, url, body, @headers)
 
-  def process(response) do
-    case response do
-      {:ok,    %HTTPoison.Response{status_code: 200, body: body}} -> IO.puts body
-      {:ok,    %HTTPoison.Response{status_code: 401}}             -> IO.puts "Given token is invalid!"
-      {:ok,    %HTTPoison.Response{status_code: 404}}             -> IO.puts "Requested resource not found!"
-      {:error, %HTTPoison.Error{reason: reason}}                  -> IO.inspect reason
-    end
-  end
+  def process({:ok, %HTTPoison.Response{status_code: 200, body: body}}),
+  do: body |> JSX.prettify! |> IO.puts
+
+  def process({:ok,%HTTPoison.Response{status_code: 401}}),
+  do: IO.puts "Given token is invalid!"
+
+  def process({:ok,%HTTPoison.Response{status_code: 404}}),
+  do: IO.puts "Requested resource not found!"
+
+  def process({:error, %HTTPoison.Error{reason: reason}}),
+  do: IO.inspect reason
 end
