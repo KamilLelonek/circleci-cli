d8a3196f82ec2d3cdf474a3375972c52edbd3e50
Kamil Lelonek <squixy.sln@gmail.com>
2015-07-05 15:07:11 +0200

    Calling HTTP poison on request

--- lib/circleci_cli/http/request.ex
+++ lib/circleci_cli/http/request.ex
@@ -1,4 +1,4 @@
 defmodule CircleciCli.HTTP.Request do
-  def send([url: url, method: method, body: body]) do
-  end
+  def send([url: url, method: method, body: body]),
+  do: HTTPoison.request(method, url, body)
 end
